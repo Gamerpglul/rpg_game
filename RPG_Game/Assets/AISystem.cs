a4b8bb5a506b0c45891fdc8a7d1acde2c3eb4d3f
Marcel <Marcelotto2@gmx.de>
2018-07-13 18:28:58 +0200

    New

---
+++ RPG_Game/Assets/AISystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISystem : MonoBehaviour
+{
+
+    bool IdelAktiv = false;
+    bool FightAktiv = false;
+    public float SpottingRange = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IdelAktiv == true)
+        {
+
+        }
+        else if (FightAktiv == true)
+        {
+
+        }
+        else
+        {
+            WasZuTun();
+        }
+    }
+
+    //Schauen Ob gegner in reichweite ist(Fight) oder nicht(Idel)
+    void WasZuTun()
+    {
+
+    }
+
+    void Idel()
+    {
+
+    }
+
+    void Fight()
+    {
+
+    }
+}
---
+++ RPG_Game/Assets/AllPlayers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllPlayers : MonoBehaviour {
+
+
+    public GameObject[] Allplayers;
+	// Use this for initialization
+	void Start () {
+       Allplayers = GameObject.FindGameObjectsWithTag("Player");
+	}
+
+}
